48bf40508ba872bc4429ce6d33db4b9c327a4eec
Slerbamonsteri <75551384+Slerbamonsteri@users.noreply.github.com>
2020-12-27 06:21:20 +0200

    Update client.lua

--- esx_selldrugsv2/client.lua
+++ esx_selldrugsv2/client.lua
@@ -83,7 +83,7 @@ end
 
 Citizen.CreateThread(function()
 	while ESX == nil do
-		TriggerEvent('pkrp:getSharedObject', function(obj) ESX = obj end)
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
 	end
 
