1ae862cd3565add0f7ac39eef0c6dcde7cf72d13
Slerbamonsteri <75551384+Slerbamonsteri@users.noreply.github.com>
2020-12-27 06:05:39 +0200

    Add files via upload

---
+++ esx_selldrugsv2/fxmanifest.lua
@@ -0,0 +1,21 @@
+--##################################################--
+--				Original file : stasiek_selldrugsv2											--
+--				Originally made by stasiek														--
+--		https://github.com/xxxstasiek/stasiek_selldrugsv2						--
+--																														--
+--			Modified and customized by Slerbamonsteri							--
+--					Discord: Slerbamonsteri#5636											--
+--##################################################--
+fx_version 'adamant'
+
+game 'gta5'
+
+client_scripts{
+    'config.lua',
+    'client.lua',
+}
+
+server_scripts{
+    'config.lua',
+    'server.lua',
+}
